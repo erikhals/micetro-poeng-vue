5d7d8dec918299f5bb7b3fe96246e4a615e3d75e
Erik Hals <erikhals@gmail.com>
2017-08-25 16:19:55 +0200

    Working on signin

--- src/components/User/SignIn.vue
+++ src/components/User/SignIn.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    <p>The Sign-in page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 md6 offset-md3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required>
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn large primary type="submit">Sign in</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        email: 'erikhals@gmail.com',
+        password: ''
+      }
+    },
+    methods: {
+      onSignup () {
+        // Vue
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 
 Vue.use(Vuetify)
@@ -10,5 +11,14 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDA3OKvkpat-9LFYSG0GnfY2xrfpLmz4aE',
+      authDomain: 'micetro-poeng.firebaseapp.com',
+      databaseURL: 'https://micetro-poeng.firebaseio.com',
+      projectId: 'micetro-poeng',
+      storageBucket: 'micetro-poeng.appspot.com'
+    })
+  }
 })
